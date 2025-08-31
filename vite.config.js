38727711bd92115027c56b0df66827a351b499b3
Zephyr <Zhangp0830@163.com>
2025-09-01 01:24:18 +1000

    fix vite setting file

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,16 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://13.239.237.65:8000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
